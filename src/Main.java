d1575bc153b3399898a88a01f9230356977e8f63
Your Name <you@example.com>
2022-04-19 20:01:11 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "Kwiecień";
+        int year = 2022;
+        long day = 19;
+        System.out.println("Miesiąc:" + month);
+        System.out.println("Dzien:" + day);
+        System.out.println("Rok:" + year);
+    }
+}
