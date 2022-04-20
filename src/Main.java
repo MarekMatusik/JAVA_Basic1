df06b91c65a9111e48b67432819fd9295191d564
Your Name <you@example.com>
2022-04-20 17:44:01 +0200

    delete

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String month = "Kwiecień";
-        int year = 2022;
-        long day = 19;
-        System.out.println("Miesiąc:" + month);
-        System.out.println("Dzien:" + day);
-        System.out.println("Rok:" + year);
+        System.out.println("Jak masz na imię?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String surename = scanner.nextLine();
+        System.out.println("Ile masz lat?");
+        String year = scanner.nextLine();
+        System.out.println("Cześć " + name + " " + surename + " lat:" + year);
     }
 }
