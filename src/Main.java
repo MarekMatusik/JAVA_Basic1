bf3bb53449bf8e376117baba2bd8b7d02d1a23cf
Your Name <you@example.com>
2022-04-20 20:18:46 +0200

    Java Basics Loop Training

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jak masz na imię?");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Jak masz na nazwisko?");
-        String surename = scanner.nextLine();
-        System.out.println("Ile masz lat?");
-        String year = scanner.nextLine();
-        System.out.println("Cześć " + name + " " + surename + " lat:" + year);
+//       int a = 3;
+//       int b = 4;
+//       a += 8;
+//       a ++;
+//       b %= 3;
+//       System.out.println(a);
+//       System.out.println(b);
+
+//    int [] TabelaCyfr = new int[5];
+//    TabelaCyfr = new int[]{1, 2, 3, 4, 5};
+//    System.out.println(TabelaCyfr[1]);
+
+
+    String [] TabelaPieskow = new String[4];
+    TabelaPieskow = new String[]{"azor", "boss", "king", "kudlaty"};
+    System.out.println(TabelaPieskow[3]);
+
+
+    for (int i=0; i < TabelaPieskow.length; i++){
+        System.out.println(TabelaPieskow[i]);
+
+
+
+    }
+
+
     }
 }
